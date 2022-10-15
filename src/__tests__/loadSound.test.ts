f444f3b38ed7bec161b6028139609c4678654106
Amy Pellegrini <amyvpellegrini@gmail.com>
2022-10-15 23:01:51 +0200

    feat: loadSound helper function
    * npm init
    
    * Configure typescript build and add deps
    
    * ci: configure semantic release
    
    * ci: setup test command and github actions
    
    * ci: upgrade actions version (Node.js)
    
    * ci: remove prepare command / fix pipeline issue
    
    * ci: add passing test (green build)
    
    * feat: leadSound helper function & jest configuration
    
    * ci: exclude test files from build

---
+++ src/__tests__/loadSound.test.ts
@@ -0,0 +1,74 @@
+import { Howl } from "howler";
+import loadSound from "../loadSound";
+
+function MockHowl({ src, onload, onloaderror }) {
+  if (src === "valid-source.mp3") {
+    setTimeout(() => {
+      onload();
+    }, 500);
+  }
+
+  if (src === "fail-source.mp3") {
+    setTimeout(() => {
+      onloaderror("123", "There was an error while loading the sound.");
+    }, 500);
+  }
+}
+
+jest.mock("howler", () => {
+  return {
+    Howl: MockHowl,
+  };
+});
+
+describe("loadSound", () => {
+  it("returns the promise of a loaded sound", () => {
+    const promise = loadSound({
+      src: "valid-source.mp3",
+    });
+
+    expect(promise).toBeInstanceOf(Promise);
+  });
+
+  it("returnes the resolved sound", async () => {
+    const howl = await loadSound({
+      src: "valid-source.mp3",
+    });
+
+    expect(howl).toBeInstanceOf(Howl);
+  });
+
+  it("rejects when failing to load the sound", async () => {
+    expect.assertions(1);
+
+    await expect(
+      loadSound({
+        src: "fail-source.mp3",
+      })
+    ).rejects.toEqual("There was an error while loading the sound.");
+  });
+
+  it('should call "onload" handler if provided', async () => {
+    const mockOnload = jest.fn();
+
+    await loadSound({
+      onload: mockOnload,
+      src: "valid-source.mp3",
+    });
+
+    expect(mockOnload).toHaveBeenCalled();
+  });
+
+  it('should call "onloaderror" handler if provided', async () => {
+    const mockOnLoadError = jest.fn();
+
+    try {
+      await loadSound({
+        onloaderror: mockOnLoadError,
+        src: "fail-source.mp3",
+      });
+    } catch (error) {
+      expect(mockOnLoadError).toHaveBeenCalled();
+    }
+  });
+});
